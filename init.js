e097cbab4b1a569bbcc76df3e9bf06b73f31b458
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-21 23:24:39 +0300

    Code indentation

--- init.js
+++ init.js
@@ -1,21 +1,21 @@
 
 // Add module 'ui.router' as dependency.
 Meteor.startup(function() {
-    if (typeof(ngMeteor) !== 'undefined') {
-        ngMeteor.requires.push('ui.router');
-    }
+  if (typeof(ngMeteor) !== 'undefined') {
+    ngMeteor.requires.push('ui.router');
+  }
 });
 
 // Exported.
 UiRouter = {
-    template: function(templName) {
-        return function() {
-            // test for pre 0.8.3
-            if (Template[templName].render)
-              return Template[templName].render().toHTML();
-            else
-              return Blaze.toHTML(Template[templName]);
-        }
+  template: function(templName) {
+    return function() {
+      // test for pre 0.8.3
+      if (Template[templName].render)
+        return Template[templName].render().toHTML();
+      else
+        return Blaze.toHTML(Template[templName]);
     }
+  }
 }
 
