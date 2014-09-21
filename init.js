d935b3f22dd6c03da78c66fec52ffd37bb7240c8
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-22 00:09:41 +0300

    Removing auto include dependency so the initialization will be as similar to regular angular as possible

--- init.js
+++ init.js
@@ -1,11 +1,3 @@
-
-// Add module 'ui.router' as dependency.
-Meteor.startup(function() {
-  if (typeof(ngMeteor) !== 'undefined') {
-    ngMeteor.requires.push('ui.router');
-  }
-});
-
 // Exported.
 UiRouter = {
   template: function(templName) {
